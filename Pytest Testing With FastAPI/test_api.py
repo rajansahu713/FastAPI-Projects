70f2f7c50e79912ba2e16ab692b5040ac421bed8
Rajan <rajansahu713@gmail.com>
2024-04-06 23:40:07 +0530

    Pytest Testing with fastAPI

---
+++ Pytest Testing With FastAPI/test_api.py
@@ -0,0 +1,97 @@
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, StaticPool
+from sqlalchemy.orm import sessionmaker
+from .models import Base
+from .main import app,get_db
+ 
+
+# Setup the TestClient
+client = TestClient(app)
+
+# Setup the in-memory SQLite database for testing
+DATABASE_URL = "sqlite:///:memory:"
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "check_same_thread": False,
+    },
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
+
+# Dependency to override the get_db dependency in the main app
+def override_get_db():
+    database = TestingSessionLocal()
+    try:
+        yield database
+    finally:
+        database.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == "server is running"
+
+
+def test_create_item():
+    response = client.post(
+        "/items/", json={"name": "Test Item", "description": "This is a test item"}
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["name"] == "Test Item"
+    assert data["description"] == "This is a test item"
+    assert "id" in data
+
+
+def test_read_item():
+    # Create an item
+    response = client.post(
+        "/items/", json={"name": "Test Item", "description": "This is a test item"}
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    item_id = data["id"]
+
+    response = client.get(f"/items/{item_id}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["name"] == "Test Item"
+    assert data["description"] == "This is a test item"
+    assert data["id"] == item_id
+
+
+def test_update_item():
+    item_id = 1
+    response = client.put(
+        f"/items/{item_id}",
+        json={"name": "Updated Item", "description": "This is an updated item"},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["name"] == "Updated Item"
+    assert data["description"] == "This is an updated item"
+    assert data["id"] == item_id
+
+
+def test_delete_item():
+    item_id = 1
+    response = client.delete(f"/items/{item_id}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["id"] == item_id
+    # Try to get the deleted item
+    response = client.get(f"/items/{item_id}")
+    assert response.status_code == 404, response.text
+
+
+
+
+
+def teardown() -> None:
+    Base.metadata.drop_all(bind=engine)
